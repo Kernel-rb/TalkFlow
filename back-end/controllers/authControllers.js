f66c836bb5818dc56c0b99b31b8512ea2fdb76c0
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-08 15:11:38 +0100

    Add bcrypt password hashing and token generation

--- back-end/controllers/authControllers.js
+++ back-end/controllers/authControllers.js
@@ -1,4 +1,6 @@
 import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+import generateTokenandSetCookie from "../utils/generateToken.js";
 
 export const signupUser = async (req, res) => {
     try {
@@ -15,22 +17,31 @@ export const signupUser = async (req, res) => {
             return res.status(400).json({ message: "Username already exists" });
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         const profilePic = `https://avatar.iran.liara.run/username?username=${username}`;
 
         const newUser = new User({
             fullname: fullName,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic,
         });
-        await newUser.save();
-        res.status(201).json({
-            _id: newUser._id,
-            fullname: newUser.fullname,
-            username: newUser.username,
-            gender: newUser.gender
-        });
+        if (newUser) {
+            generateTokenandSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                username: newUser.username,
+                gender: newUser.gender,
+                profilePic: newUser.profilePic
+            });
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
 
     } catch (error) {
         console.log(error);
