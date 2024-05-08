e5a2c33441fcaa9d19ca1f8554be6984bc310ff6
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-08 10:39:48 +0100

    Refactor server.js to use arrow function syntax for route handlers

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,13 @@ const express = require("express");
 const { chat } = require("./data/data");
 
 const app = express();
+dotenv.config();
+
 const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.get("/", (req, res) => {res.send("Hello World");});
 
-app.get("/api/chat", (req, res) => {
-    res.send(chat);
-});
+app.get("/api/chat", (req, res) => {res.send(chat);});
 
 app.get("/api/chat/:id", (req, res) => {
     const singleChat = chat.find((user) => user._id === req.params.id);
