88348765921751ef6d3607fcb401c354eab1b0b1
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-09 21:09:11 +0100

    Update .gitignore and server.js, fix typo in package.json

--- backend/sever.js
+++ backend/sever.js
@@ -1,12 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes);
+
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
