d8f5111feed8fc41a2584b717bbdd527c3d261b8
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-10 09:13:06 +0100

    Refactor protectedRoute middleware and import statement in auth.routes.js

--- backend/middleware/protectedRoute.js
+++ backend/middleware/protectedRoute.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const protectedRoute = async (req, res, next) => {
     try {
-        const token = req.cookies.token;
+        const token = req.cookies.jwt;
         if (!token) return res.status(401).json({ message: "Unauthorized." });
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         if (!decoded) return res.status(401).json({ message: "Unauthorized : invalid token" });
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-
-import { register, login, logout , me} from "../controllers/auth.controller.js";
+import { register, login, logout, me } from "../controllers/auth.controller.js";
+import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const router = express.Router();
 
