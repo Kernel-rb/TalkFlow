c33f532f67f09d7e2d10db13ba872fdd5940b800
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-12 21:10:30 +0100

    feat: Implement deletePost functionality

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -27,7 +27,23 @@ export const createPost = async (req, res) => {
         return res.status(500).json({ message: "Internal server error" });
     }
  };
-export const deletePost = async (req, res) => { };
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({ message: "Post not found" });
+        if (post.user.toString() !== req.user._id.toString()) return res.status(401).json({ message: "Unauthorized" });
+        if (post.image) {
+            const imageId = post.image.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(imageId);
+        }
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Post deleted successfully" });
+    }catch(error){
+        console.log("Error in deletePost: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
+ };
 export const commentOnPost = async (req, res) => { };
 export const likeUnlikePost = async (req, res) => { };
 export const getAllPosts = async (req, res) => { };
