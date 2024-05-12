aa2f5b9b5af9baf52d04773db0b9f0d86f10ed18
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-12 14:45:39 +0100

    chore: Add post routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
+import connectDB from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import connectDB from "./db/connectDB.js";
-import cookieParser from "cookie-parser";
-import {v2 as cloudinary} from "cloudinary";
+import postRoutes from "./routes/post.routes.js";
 
 
 dotenv.config();
@@ -25,6 +27,7 @@ app.use(cookieParser()); // get cookies from request
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
