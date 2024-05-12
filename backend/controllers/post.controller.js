bf0181708b1ae84822dcb358533a652c5c6f4a8e
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-12 21:30:29 +0100

    feat: Add comment functionality to post controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -44,7 +44,30 @@ export const deletePost = async (req, res) => {
     }
 
  };
-export const commentOnPost = async (req, res) => { };
+export const commentOnPost = async (req, res) => { 
+    try {
+        const { text } = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+
+        if (!text) return res.status(400).json({ message: "Text is required" });
+        
+        const post = await Post.findById(postId);
+        if (!post) return res.status(404).json({ message: "Post not found" });
+
+        const comment = {
+            user: userId,
+            text,
+        }
+        post.comments.push(comment);
+        await post.save();
+        res.status(201).json(post);
+    } catch (error) {
+        console.log("Error in commentOnPost: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+    
+    }
+};
 export const likeUnlikePost = async (req, res) => { };
 export const getAllPosts = async (req, res) => { };
 export const getLikedPosts = async (req, res) => { };
