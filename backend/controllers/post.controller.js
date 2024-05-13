8f074b5c892e5784b65820412097519ac5f9715e
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-13 11:09:31 +0100

    feat: Add getFollowingPosts and getUserPosts functionality to post controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -135,11 +135,47 @@ export const getLikedPosts = async (req, res) => {
             select: "-password",
         });
         res.status(200).json(likedPost);
-        
+
     } catch (error) {
         console.log("Error in getLikedPosts: ", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 };
-export const getFollowingPosts = async (req, res) => { };
-export const getUserPosts = async (req, res) => { };
+export const getFollowingPosts = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ message: "User not found" });
+        const following = user.following;
+        const feedPosts = await Post.find({ user: { $in: following } }).sort({ createdAt: -1 }).populate({
+            path: "user",
+            select: "-password",
+        }).populate({
+            path: "comments.user",
+            select: "-password",
+        });
+        res.status(200).json(feedPosts);
+    } catch (error) {
+        console.log("Error in getFollowingPosts: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+ };
+export const getUserPosts = async (req, res) => {
+    try {
+        const { username } = req.params;
+        const user = await User.find({ username });
+        if (!user) return res.status(404).json({ message: "User not found" });
+        
+        const posts = await Post.find({ user: user._id }).sort({ createdAt: -1 }).populate({
+            path: "user",
+            select: "-password",
+        }).populate({
+            path: "comments.user",
+            select: "-password",
+        });
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log("Error in getUserPosts: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+ };
