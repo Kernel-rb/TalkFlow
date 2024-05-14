ec956c5d1a89a35fe7188693e75996749423b66d
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-14 17:49:07 +0100

    fix: Remove commented out code and fix typo in ProfilePage.jsx

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -222,6 +222,7 @@ const ProfilePage = () => {
               </div>
               <div className="flex w-full border-b border-gray-700 mt-4">
                 <div
+                  //TODO: fi the bug ith the posts
                   className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 relative cursor-pointer"
                   onClick={() => setFeedType("posts")}
                 >
