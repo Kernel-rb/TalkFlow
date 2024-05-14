761f1c34a66e76818309d52f87fc040529f4bc81
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-14 17:00:54 +0100

    chore: Update express.json() limit to 5mb for request body parsing

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(express.json());
+app.use(express.json("limit: 5mb"));
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser()); // get cookies from request
 
